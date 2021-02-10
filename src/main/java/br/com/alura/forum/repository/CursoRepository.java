5f57f5264fc7c7462cd933e718186d800b2720c1
LarissaZanata <zanatalarissamorais@gmail.com>
2021-02-09 23:41:16 -0300

    Implementação de Api Rest para forum de dúvidas

---
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+public interface CursoRepository extends JpaRepository<Curso, Long>{
+
+	Curso findByNome(String nomeCurso);
+	
+}
